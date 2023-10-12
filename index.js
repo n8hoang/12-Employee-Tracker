869c6e1f632a566af491be9e31114a62efc25984
Nathan Hoang <sliqq123@gmail.com>
2023-10-12 16:00:00 -0700

    added add new role function

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function addNewDepartment() {
     inquirer.prompt({
         type: "input",
         name: "departmentName",
-        message: "Enter the name of the new department:"
+        message: "What is the name of the new department?"
     }).then(answer => {
         db.query(`INSERT INTO department (name) VALUES ('${answer.departmentName}')`, (err, res) => {
                 if (err) throw err;
@@ -57,8 +57,60 @@ function addNewDepartment() {
         );
     });
 }
+async function addNewRole() {
+    try {
+        const [departments] = await db.promise().query("SELECT * FROM department");
 
+        if (!departments.length) {
+            console.error("No departments found.");
+            return start();
+        }
 
+        const answer = await inquirer.prompt([
+            {
+                type: "input",
+                name: "roleTitle",
+                message: "Enter the title of the new role:"
+            },
+            {
+                type: "input",
+                name: "roleSalary",
+                message: "Enter the salary for this role:",
+                validate: value => {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return "Please enter a valid number.";
+                }
+            },
+            {
+                type: "list",
+                name: "departmentName",
+                message: "Which department does this role belong to?",
+                choices: departments.map(department => department.name)
+            }
+        ]);
+
+        const department = departments.find(dept => dept.name === answer.departmentName);
+
+        if (!department) {
+            console.error("Matching department not found.");
+            return mainPrompt();
+        }
+
+        await db.promise().query(
+            "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+            [answer.roleTitle, parseFloat(answer.roleSalary), department.id]
+        );
+
+        console.log(`Added new role: ${answer.roleTitle}`);
+        mainPrompt();
+
+    } catch (err) {
+        console.error(err);
+        mainPrompt();
+    }
+}
 
 function mainPrompt() {
     inquirer.prompt([
@@ -91,11 +143,11 @@ function mainPrompt() {
             case "Add a department":
                 addNewDepartment();
                 break;
-            case "Add a department":
-                addDepartment();
+            case "Add a role":
+                addNewRole();
                 break;
-            case "Add a department":
-                addDepartment();
+            case "Add a employee":
+                addNewEmployee();
                 break;
             case "Add a department":
                 addDepartment();
