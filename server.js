8d2afa821db52da4f6f43ad81dabe26fd225c630
Nathan Hoang <sliqq123@gmail.com>
2023-10-12 13:44:28 -0700

    added required packages and boiler plate for server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT || 3001;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
